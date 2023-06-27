95ac122e20c88fd7302d946d602b92a074246d38
dotPast <83116565+dotPast@users.noreply.github.com>
2023-06-27 11:01:57 +0300

    Slowly updating the plugin

--- src/main/java/org/dotpast/XPUtils.kt
+++ src/main/java/org/dotpast/XPUtils.kt
@@ -3,11 +3,13 @@ package org.dotpast
 import org.bukkit.plugin.java.JavaPlugin
 import org.dotpast.commands.CurrentExp
 import org.dotpast.commands.ExportExp
+import org.dotpast.commands.TransferExp
 
 class XPUtils : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
         getCommand("exportxp")?.setExecutor(ExportExp())
         getCommand("currentxp")?.setExecutor(CurrentExp())
+        getCommand("transferxp")?.setExecutor(TransferExp())
     }
 }
